6824ac80dcc0e6d9c119e0880f3a3398ceedee3b
LuckyF1re <andruha.sadovich@gmail.com>
2025-04-30 22:21:08 +0300

    First

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div>xx xx xx</div>
+  )
+}
+
+export default App
